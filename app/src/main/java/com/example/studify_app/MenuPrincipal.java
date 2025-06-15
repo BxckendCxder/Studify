0ffc61cfb743266a3aad5ad68e7b87b33e472974
D3CL0AK <daniel@gmail.com>
2025-06-14 23:44:29 -0600

    PRUEBA

--- app/src/main/java/com/example/studify_app/MenuPrincipal.java
+++ app/src/main/java/com/example/studify_app/MenuPrincipal.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuPrincipal extends AppCompatActivity {
     //MENU PRINCIPAL DE LA APP
-    //PRUEBA
+    //PRUEBA 1
     Button btnRegistroMaterias, btnRegistroActividades, btnControlCalificaciones,
             btnMaterias,btnActividades,btnCalificaciones,btnSalir;
 
